b83355d25f97c9d8228c00fbd08751835c552416
Jose Vasquez <143973619+Xoserius@users.noreply.github.com>
2024-04-06 02:57:09 -0400

    Add files via upload

--- Sistema de control de inventario/Qt designer/Main.py
+++ Sistema de control de inventario/Qt designer/Main.py
@@ -2,15 +2,18 @@ import sys
 import sqlite3
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from PyQt5.QtGui import QImage
 
 
-class Ui_ventana_inicio(QMainWindow):
+class Ui_ventana_inicio(QMainWindow): #clase creada para la ventana de Inicio 
     
     def __init__(self):
-        super().__init__()
+        # Inicializa una nueva instancia de la clase.
+        super().__init__() 
+        # Llama al constructor de la clase base
+        loadUi("1.Inicio.ui", self) 
+        # Carga la interfaz de usuario desde el archivo
 
-        # Cargar el archivo .ui
-        loadUi("1.Inicio.ui", self)
         # Conectar el botón "Iniciar sesión" a la función para abrir la ventana de inicio de sesión
         self.pushButton_iniciarsesion.clicked.connect(self.abrir_ventana_login)
         self.pushButton_Salir.clicked.connect(self.salir)
@@ -29,12 +32,12 @@ class Ui_ventana_inicio(QMainWindow):
         self.ventana_login = Ui_Qdialog_login(self.conexion_db, self.ventana_menu_principal)
         self.ventana_login.show()
 
-class Ui_Qdialog_login(QDialog):
+class Ui_Qdialog_login(QDialog): #clase creada para la ventana de Login 
     
     def __init__(self, conexion_db, ventana_menu_principal):
         super().__init__()
-        # Cargar el archivo .ui
         loadUi("2.Login.ui", self)
+        # Carga la interfaz de usuario desde el archivo
 
         self.conexion_db = conexion_db
         self.ventana_menu_principal = ventana_menu_principal
@@ -64,13 +67,14 @@ class Ui_Qdialog_login(QDialog):
             # Si la consulta no devuelve resultados, mostrar un mensaje de error
             QMessageBox.warning(self, "Error", "Usuario o clave incorrectos.")
 
-class Ui_QMainwindow_Menu_principal(QMainWindow):
+class Ui_QMainwindow_Menu_principal(QMainWindow): #clase creada para la ventana de menu principal 
     
     def __init__(self):
-        super().__init__()
-        # Cargar el archivo .ui
+        super().__init__() 
         loadUi("3.Menu_principal.ui", self)
+        # Carga la interfaz de usuario desde el archivo
 
+        # Conectar botones a funciones
         self.pushButton_salir_menu.clicked.connect(self.salir)
         self.pushButton_gestiondeusuario.clicked.connect(self.abrir_ventana_gestion)
         self.pushButton_ingresarventa.clicked.connect(self.abrir_ingresar_ventas)
@@ -78,7 +82,7 @@ class Ui_QMainwindow_Menu_principal(QMainWindow):
         self.pushButton_anadir_eliminar.clicked.connect(self.abrir_ajustes)
       
 
-
+        # Instanciar ventanas
         self.ventana_gestionusu = Ui_QMainwindow_Gestion_de_usuarios(QMainWindow)
         self.ventana_ingresarven = Ui_QMainwindow_Ingresar_ventas(QMainWindow)
         self.ventana_report = Ui_QMainwindow_Reportes(QMainWindow)
@@ -86,63 +90,131 @@ class Ui_QMainwindow_Menu_principal(QMainWindow):
        
 
     def abrir_ventana_gestion(self):
-        # Mostrar la ventana de inicio de sesión
+        # metodo para mostrar la ventana de gestion de usuarios
         self.ventana_gestionusu = Ui_QMainwindow_Gestion_de_usuarios(self.ventana_gestionusu)
-        self.ventana_gestionusu.show()
+        # Instanciar ventana
+        self.ventana_gestionusu.showFullScreen()
+        # Mostrar ventana en pantalla completa
 
     def abrir_ingresar_ventas(self):
-        # Mostrar la ventana de inicio de sesión
+        # metodo para mostrar la ventana de ingresar ventas
         self.ventana_ingresarven = Ui_QMainwindow_Ingresar_ventas(self.ventana_ingresarven)
-        self.ventana_ingresarven.show()
+        # Instanciar ventana
+        self.ventana_ingresarven.showFullScreen()
+        # Mostrar ventana en pantalla completa
 
     def abrir_reportes (self):
-        # Mostrar la ventana de inicio de sesión
+        # Mostrar la ventana de Reportes
         self.ventana_report = Ui_QMainwindow_Reportes(self.ventana_report)
-        self.ventana_report.show()
+        # Instanciar ventana
+        self.ventana_report.showFullScreen()
+        # Mostrar ventana en pantalla completa
     
-    def abrir_ajustes (self):# Mostrar la ventana de inicio de sesión
+    def abrir_ajustes (self):# Mostrar la ventana de añadir/eliminar
         self.ventana_ajustes = Ui_QMainwindow_Ajustes_anadir_eliminar(self.ventana_ajustes)
-        self.ventana_ajustes.show()
+        # Instanciar ventana
+        self.ventana_ajustes.showFullScreen()
+        # Mostrar ventana en pantalla completa
 
     def salir(self):
         self.close()
+        # Cierra la ventana actual. Este método es llamado cuando se hace clic en el botón 'regresar' en la interfaz gráfica. Cierra la ventana actual.
 
-class Ui_QMainwindow_Gestion_de_usuarios(QMainWindow):
+class Ui_QMainwindow_Gestion_de_usuarios(QMainWindow): #clase creada para la ventana de gestion de usuarios 
     
     def __init__(self, ventana_gestiondeusuarios):
         super().__init__()
-        # Cargar el archivo .ui
         loadUi("4.Gestion_de_usuarios.ui", self)
+        # Carga la interfaz de usuario desde el archivo
 
+        self.pushButton_regresar_gestion.clicked.connect(self.salir)
+        # Crear una instancia de la ventana de gestion de usuarios
         self.ventana_gestiondeusu = ventana_gestiondeusuarios
 
+        
+        # Conexiones de los botones a sus respectivos métodos para mostrar páginas
+        self.pushButton_usuarios.clicked.connect(self.mostrar_pagina_usuarios)
+        self.pushButton_anadir.clicked.connect(self.mostrar_pagina_anadir)
+        self.pushButton_eliminar.clicked.connect(self.mostrar_pagina_eliminar)
+        self.pushButton_actualizar.clicked.connect(self.mostrar_pagina_actualizar)
+      
+    # Metodos para mostrar las paginas
+    def mostrar_pagina_usuarios(self):
+       self.stackedWidget.setCurrentIndex(0)
+    def mostrar_pagina_actualizar(self):
+        self.stackedWidget.setCurrentIndex(1)
+    def mostrar_pagina_eliminar(self):
+        self.stackedWidget.setCurrentIndex(2)    
+    def mostrar_pagina_anadir(self):
+        self.stackedWidget.setCurrentIndex(3)
+             
 
-class Ui_QMainwindow_Ingresar_ventas(QMainWindow):
+    def salir(self):
+        self.close()
+        # Cierra la ventana actual. Este método es llamado cuando se hace clic en el botón 'regresar' en la interfaz gráfica. Cierra la ventana actual.
+
+class Ui_QMainwindow_Ingresar_ventas(QMainWindow): #clase creada para la ventana de ingresar ventas 
     
     def __init__(self, ventana_ingresarventas):
         super().__init__()
-        # Cargar el archivo .ui
         loadUi("5.Ingresar_ventas.ui", self)
+        # Carga la interfaz de usuario desde el archivo
+        self.pushButton_regresar_iv.clicked.connect(self.salir)
+        # Crear una instancia de la ventana de ingresar ventas
         self.ventana_ingresarventas = ventana_ingresarventas
 
-class Ui_QMainwindow_Reportes(QMainWindow):
+    def salir(self):
+        self.close()
+        # Cierra la ventana actual. Este método es llamado cuando se hace clic en el botón 'regresar' en la interfaz gráfica. Cierra la ventana actual.    
+
+class Ui_QMainwindow_Reportes(QMainWindow): #clase creada para la ventana de Reportes 
     
     def __init__(self, ventana_report):
         super().__init__()
-        # Cargar el archivo .ui
         loadUi("6.Reportes.ui", self)
+        # Carga la interfaz de usuario desde el archivo
+
+        # Conectar botones a funciones
+        self.pushButton_regresar_rep.clicked.connect(self.salir)
+        # Crear una instancia de la ventana de Reportes
         self.ventana_report = ventana_report
 
-class Ui_QMainwindow_Ajustes_anadir_eliminar(QMainWindow):
+        # Conexiones de los botones a sus respectivos métodos para mostrar páginas
+        self.pushButton_stock.clicked.connect(self.mostrar_pagina_stock)
+        self.pushButton_Ventas.clicked.connect(self.mostrar_pagina_ventas)
+        
+      
+    # Metodos para mostrar las paginas
+    def mostrar_pagina_stock(self):
+       self.stackedWidget.setCurrentIndex(0)
+    def mostrar_pagina_ventas(self):
+        self.stackedWidget.setCurrentIndex(1)
+    
+
+    def salir(self):
+        self.close()
+        # Cierra la ventana actual. Este método es llamado cuando se hace clic en el botón 'regresar' en la interfaz gráfica. Cierra la ventana actual.
+
+class Ui_QMainwindow_Ajustes_anadir_eliminar(QMainWindow): #clase creada para la ventana de Añadir/eliminar 
     
     def __init__(self, ventana_ajustes):
         super().__init__()
         # Cargar el archivo .ui
         loadUi("7.Ajustes_anadir_eliminar.ui", self)
+         # Conectar botones a funciones
+        self.pushButton_regresar_ajus.clicked.connect(self.salir)
+        # Instanciar ventanas
         self.ventana_ajustes = ventana_ajustes
+
+    def salir(self):
+        self.close()
+        # Cierra la ventana actual. Este método es llamado cuando se hace clic en el botón 'regresar' en la interfaz gráfica. Cierra la ventana actual.
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # Punto de entrada principal de la aplicación.
     ventana = Ui_ventana_inicio()
-    ventana.showFullScreen() # Muestra la ventana de inicio en pantalla completa
+    ventana.showFullScreen() 
+    # Instanciar y mostrar la ventana de inicio en pantalla completa
     sys.exit(app.exec_())
+    # Ejecutar la aplicación hasta que se cierre la ventana principal
