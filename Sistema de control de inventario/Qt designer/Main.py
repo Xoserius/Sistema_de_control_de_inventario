ef10a356cc184b356a8aaa92083bdebf0f014bde
Jose Vasquez <143973619+Xoserius@users.noreply.github.com>
2024-04-05 22:11:06 -0400

    Add files via upload

--- Sistema de control de inventario/Qt designer/Main.py
+++ Sistema de control de inventario/Qt designer/Main.py
@@ -3,6 +3,7 @@ import sqlite3
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
 
+
 class Ui_ventana_inicio(QMainWindow):
     
     def __init__(self):
@@ -15,6 +16,8 @@ class Ui_ventana_inicio(QMainWindow):
         self.pushButton_Salir.clicked.connect(self.salir)
         # Conectar a la base de datos
         self.conexion_db = sqlite3.connect("Base de datos proyecto.db")
+        # Crear una instancia de la ventana de menú principal
+        self.ventana_menu_principal = Ui_QMainwindow_Menu_principal()
 
     def salir(self):
         # Cerrar la conexión a la base de datos antes de salir
@@ -23,17 +26,18 @@ class Ui_ventana_inicio(QMainWindow):
 
     def abrir_ventana_login(self):
         # Mostrar la ventana de inicio de sesión
-        self.ventana_login = Ui_Qdialog_login(self.conexion_db)
+        self.ventana_login = Ui_Qdialog_login(self.conexion_db, self.ventana_menu_principal)
         self.ventana_login.show()
 
 class Ui_Qdialog_login(QDialog):
     
-    def __init__(self, conexion_db):
+    def __init__(self, conexion_db, ventana_menu_principal):
         super().__init__()
         # Cargar el archivo .ui
         loadUi("2.Login.ui", self)
 
         self.conexion_db = conexion_db
+        self.ventana_menu_principal = ventana_menu_principal
 
         # Conectar el botón "Aceptar" a la función para iniciar sesión
         self.pushButton_aceptar.clicked.connect(self.iniciar_sesion)
@@ -55,8 +59,7 @@ class Ui_Qdialog_login(QDialog):
         if resultado:
             # Si la consulta devuelve resultados, abrir la ventana del menú principal
             self.close()
-            ventana_menu_principal = Ui_QMainwindow_Menu_principal()
-            ventana_menu_principal.show()
+            self.ventana_menu_principal.showFullScreen()
         else:
             # Si la consulta no devuelve resultados, mostrar un mensaje de error
             QMessageBox.warning(self, "Error", "Usuario o clave incorrectos.")
@@ -68,10 +71,78 @@ class Ui_QMainwindow_Menu_principal(QMainWindow):
         # Cargar el archivo .ui
         loadUi("3.Menu_principal.ui", self)
 
+        self.pushButton_salir_menu.clicked.connect(self.salir)
+        self.pushButton_gestiondeusuario.clicked.connect(self.abrir_ventana_gestion)
+        self.pushButton_ingresarventa.clicked.connect(self.abrir_ingresar_ventas)
+        self.pushButton_reportes.clicked.connect(self.abrir_reportes)
+        self.pushButton_anadir_eliminar.clicked.connect(self.abrir_ajustes)
+      
+
+
+        self.ventana_gestionusu = Ui_QMainwindow_Gestion_de_usuarios(QMainWindow)
+        self.ventana_ingresarven = Ui_QMainwindow_Ingresar_ventas(QMainWindow)
+        self.ventana_report = Ui_QMainwindow_Reportes(QMainWindow)
+        self.ventana_ajustes = Ui_QMainwindow_Ajustes_anadir_eliminar(QMainWindow)
+       
+
+    def abrir_ventana_gestion(self):
+        # Mostrar la ventana de inicio de sesión
+        self.ventana_gestionusu = Ui_QMainwindow_Gestion_de_usuarios(self.ventana_gestionusu)
+        self.ventana_gestionusu.show()
+
+    def abrir_ingresar_ventas(self):
+        # Mostrar la ventana de inicio de sesión
+        self.ventana_ingresarven = Ui_QMainwindow_Ingresar_ventas(self.ventana_ingresarven)
+        self.ventana_ingresarven.show()
+
+    def abrir_reportes (self):
+        # Mostrar la ventana de inicio de sesión
+        self.ventana_report = Ui_QMainwindow_Reportes(self.ventana_report)
+        self.ventana_report.show()
+    
+    def abrir_ajustes (self):# Mostrar la ventana de inicio de sesión
+        self.ventana_ajustes = Ui_QMainwindow_Ajustes_anadir_eliminar(self.ventana_ajustes)
+        self.ventana_ajustes.show()
+
+    def salir(self):
+        self.close()
+
+class Ui_QMainwindow_Gestion_de_usuarios(QMainWindow):
+    
+    def __init__(self, ventana_gestiondeusuarios):
+        super().__init__()
+        # Cargar el archivo .ui
+        loadUi("4.Gestion_de_usuarios.ui", self)
+
+        self.ventana_gestiondeusu = ventana_gestiondeusuarios
+
+
+class Ui_QMainwindow_Ingresar_ventas(QMainWindow):
+    
+    def __init__(self, ventana_ingresarventas):
+        super().__init__()
+        # Cargar el archivo .ui
+        loadUi("5.Ingresar_ventas.ui", self)
+        self.ventana_ingresarventas = ventana_ingresarventas
+
+class Ui_QMainwindow_Reportes(QMainWindow):
+    
+    def __init__(self, ventana_report):
+        super().__init__()
+        # Cargar el archivo .ui
+        loadUi("6.Reportes.ui", self)
+        self.ventana_report = ventana_report
+
+class Ui_QMainwindow_Ajustes_anadir_eliminar(QMainWindow):
+    
+    def __init__(self, ventana_ajustes):
+        super().__init__()
+        # Cargar el archivo .ui
+        loadUi("7.Ajustes_anadir_eliminar.ui", self)
+        self.ventana_ajustes = ventana_ajustes
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ventana = Ui_ventana_inicio()
     ventana.showFullScreen() # Muestra la ventana de inicio en pantalla completa
     sys.exit(app.exec_())
-
